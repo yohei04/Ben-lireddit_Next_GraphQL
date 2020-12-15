0fe3ec8b12f1f3f77867bbda549f8af4a6cb6325
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-16 00:20:11 +0900

    chore: comment out

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,8 @@ const main = async () => {
   // const post = orm.em.create(Post, { title: "my first post" });
   // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {})
-  console.log(posts)
+  // const posts = await orm.em.find(Post, {})
+  // console.log(posts)
 };
 
 main().catch((err) => {
