98cea241414f011398785bd31b369acd651d1290
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-16 00:37:33 +0900

    fix: change file name

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants.";
+import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import path from "path";
 
