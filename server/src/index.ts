dd5ddeb9a4c34259d7f9d5d04bb8204b33bbf31b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-21 02:32:32 +0900

    feat: URQL Basics

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { MyContext } from './types';
+import cors from 'cors';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -21,6 +22,13 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
 
   app.use(
     session({
@@ -35,7 +43,7 @@ const main = async () => {
       saveUninitialized: false,
       secret: 'keyboard cat',
       resave: false,
-    }) 
+    })
   );
 
   const apolloServer = new ApolloServer({
@@ -46,7 +54,7 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
     console.log('sever started on localhost:4000');
