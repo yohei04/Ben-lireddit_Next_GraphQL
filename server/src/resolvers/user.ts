84976e275750057f16262f5237cdea9ed8f59a98
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-24 14:48:07 +0900

    feat: Register Error Handling

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Resolver,
 } from 'type-graphql';
 import argon2 from 'argon2';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 @InputType()
 class UsernamePasswordInput {
@@ -78,12 +79,19 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*');
+      user = result[0];
     } catch (err) {
       // duplicate username error
       if (err.code === '23505') {
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button,
-} from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
-import { useMutation } from 'urql';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps {}
 
-
 const Register: React.FC<registerProps> = ({}) => {
   const [, register] = useRegisterMutation();
+  const route = useRouter();
 
   return (
     <Wrapper variant={'small'}>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            route.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
