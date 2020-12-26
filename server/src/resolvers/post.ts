f64b5b81b9a94b49d5caef5cf889543cf655e4ae
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-26 18:25:28 +0900

    feat: Next.js URQL SSR

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -2,15 +2,12 @@ import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms))
-
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(
+  posts(
     @Ctx() { em }: MyContext //{}にする前はctx
   ): Promise<Post[]> {
-    await sleep(3000)
     return em.find(Post, {});
   }
 
