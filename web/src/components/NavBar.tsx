a608f5d1d70e76a7c39a39b7ca34b66862708d99
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-26 15:51:56 +0900

    feat: Logout

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import React from 'react';
 import NextLink from 'next/link';
-import { useMeQuery } from '../generated/graphql';
+import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
   let body = null;
 
@@ -28,7 +29,13 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={2}>{data.me.username}</Box>
-        <Button variant="link">logout</Button>
+        <Button
+          variant="link"
+          isLoading={logoutFetching}
+          onClick={() => logout()}
+        >
+          logout
+        </Button>
       </Flex>
     );
   }
