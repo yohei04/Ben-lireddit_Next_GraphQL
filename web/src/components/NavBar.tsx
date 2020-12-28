6c75158f161e7a932216738bbaee25e8300d3aaa
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-12-28 16:50:36 +0900

    feat: Global Error Handling

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -44,7 +44,7 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
   }
 
   return (
-    <Flex bg="tan" p={4}>
+    <Flex bg="tan" p={4} zIndex={1} position={'sticky'} top={0}>
       <Box ml={'auto'}>{body}</Box>
     </Flex>
   );
